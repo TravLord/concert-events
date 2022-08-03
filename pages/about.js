5b9cf34ff10bd4213c3ddcdfcf70fe24f6662c9f
TravLord <92835555+TravLord@users.noreply.github.com>
2022-08-03 12:53:05 -0600

    Pages and Routing

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+
+
+export default function AboutPage() {
+  return (
+    <div>
+        <h1>About</h1>
+        <h1>This app helps you keep track of concert events</h1>
+        <p>Version: 1.0.0</p>
+    </div>
+  )
+}
+
+
---
+++ pages/events/add.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function AddEventPage() {
+  return (
+    <div>
+        <h1>Add Events</h1>
+    </div>
+  )
+}
+
